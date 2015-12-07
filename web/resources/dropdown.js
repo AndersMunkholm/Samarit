7efa66c4685e5edf6dc3a2bb51b57fce29f76dc8
Anders Munkholm <kaibys@hotmail.com>
2015-12-07 15:28:57 +0100

    EventPage Ready toJavaascript fun

--- web/resources/dropdown.js
+++ web/resources/dropdown.js
@@ -10,6 +10,10 @@ function getDropdownContent() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
+function getDropdownContent2() {
+    
+    document.getElementById("myDropdown2").classList.toggle("show2");
+}
 
 
 window.onclick = function(event) { //sker hver gang der bliver clicket på et event, Det event kommer ind i functionen
@@ -24,7 +28,21 @@ window.onclick = function(event) { //sker hver gang der bliver clicket på et ev
       }
     }
   }
+  if (!event.target.matches('.dropbtn2')) { //tjekker om det event der skete er sket på dropbtn id'et
+    
+    var dropdowns = document.getElementsByClassName("dropdown2");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
 }
+
+
+
 
 
 
